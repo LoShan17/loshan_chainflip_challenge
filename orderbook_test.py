7be61253cbbeb2e0661392e3acc35d6f3c5d5f03
SAN <shamoa.daniel@gmail.com>
2024-02-06 16:21:39 +0000

    added pytest orderbook scheleton

---
+++ orderbook_test.py
@@ -0,0 +1,22 @@
+import pytest
+from orderbook import OrderBook
+
+
+def test_orderbook_limits():
+    order_book = OrderBook(base_asset="ETH", quote_asset="USDC")
+    assert True
+
+
+def test_orderbook_ranges():
+    order_book = OrderBook(base_asset="ETH", quote_asset="USDC")
+    assert True
+
+
+def test_orderbook_limit_top():
+    order_book = OrderBook(base_asset="ETH", quote_asset="USDC")
+    assert True
+
+
+def test_orderbook_price():
+    order_book = OrderBook(base_asset="ETH", quote_asset="USDC")
+    assert True
